1d4903e20fbbc71ece3692fe816c27842506ece1
leonardofx1 <leonardonunesfreitas1@gmail.com>
2023-03-30 13:00:52 -0300

    'main'

---
+++ artPedras/src/components/Main/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import main from '../../assets/style/main.scss';
+import projetos from '../../assets/style/projetos.scss'
+
+import granito from '../../assets/img/granito.webp';
+import paraVoce from '../../assets/img/paraVoce.webp';
+const Main = () => {
+
+    return (
+
+        <main>
+        <section className="container__qualidades">
+            <article className="qualidades">
+                <h3>Atendimento personalizado</h3>
+                <p>
+                    Nossa equipe está comprometida em fornecer atendimento
+                    personalizado e identificar oportunidades únicas em cada
+                    projeto.
+                </p>
+            </article>
+            <article className="qualidades">
+                <h3>Projetos Sob Medida</h3>
+                <p>
+                    Cada projeto é único e merece atenção individualizada.
+                    Por isso, trabalhamos de forma personalizada para
+                    entregar serviços sob medida para cada cliente,
+                    garantindo que seus sonhos sejam concretizados da melhor
+                    forma possível.
+                </p>
+            </article>
+        </section>
+        <div className="container__projetos">
+            <h2>Projetos</h2>
+
+            <section className="projetos">
+                <article className="projeto">
+                    <img src={granito} alt="granito"/>
+                </article>
+                <article className="projeto">
+                    <img src={granito} alt="granito"/>
+                </article>
+
+
+                <article className="projeto__corpo">
+                    <img src={granito} alt="granito"/>
+                    <h3>Granito</h3>
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Voluptas dolores voluptates esse tempore ex ea ratione
+                        dolorum quas voluptatum, commodi, officia cum, quo
+                        nostrum soluta nisi quam natus porro fugit.
+                    </div>
+                </article>
+            </section>
+        </div>
+        <section className="propraganda">
+            <img src={paraVoce} alt="anuncio"/>
+        </section>
+    </main>
+    )
+
+}
+
+export default Main
